7a992d1270299429bc66033fb3eb31e978178e37
Borislav Stoyanov <bostoy@protonmail.com>
2022-08-22 15:01:37 +0300

    ✨ Detect if the account is connected / disconnected

--- components/ManualHeader.jsx
+++ components/ManualHeader.jsx
@@ -1,15 +1,46 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { useMoralis } from "react-moralis"
 
 const ManualHeader = () => {
-    const { enableWeb3, account } = useMoralis()
+    const { enableWeb3, deactivateWeb3, account, isWeb3Enabled, Moralis } = useMoralis()
+
+    useEffect(() => {
+        if (isWeb3Enabled) return
+        if (typeof window !== "undefined") {
+            if (window.localStorage.getItem("connected")) {
+                enableWeb3()
+            }
+        }
+
+        console.log("Is web3 enabled: ", isWeb3Enabled)
+    }, [isWeb3Enabled, enableWeb3])
+
+    useEffect(() => {
+        Moralis.onAccountChanged((account) => {
+            console.log("Account changed to: ", account)
+            if (account === null) {
+                window.localStorage.removeItem("connected")
+                deactivateWeb3()
+                console.log("Disconnected..")
+            }
+        })
+    }, [])
 
     return (
         <div>
             {account ? (
                 <div>Connected to {account}</div>
             ) : (
-                <button onClick={async () => await enableWeb3()}>Connect</button>
+                <button
+                    onClick={async () => {
+                        await enableWeb3()
+                        if (typeof window !== "undefined") {
+                            window.localStorage.setItem("connected", "injected ")
+                        }
+                    }}
+                >
+                    Connect
+                </button>
             )}
         </div>
     )
