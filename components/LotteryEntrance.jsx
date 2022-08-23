4524f600c28c5cfff50e3166a6122a2444cb8802
Borislav Stoyanov <bostoy@protonmail.com>
2022-08-23 18:58:22 +0300

    🚀 Get the Raffle entrance fee / Calling Smart Contract functions from Frontend

---
+++ components/LotteryEntrance.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react"
+import { useWeb3Contract } from "react-moralis"
+import { abi, contractAddresses } from "../constants"
+import { useMoralis } from "react-moralis"
+import { ethers } from "ethers"
+const LotteryEntrance = () => {
+    const { chainId: chainIdHex, isWeb3Enabled } = useMoralis() //gives the hex version of the id -> moralis gets it from the connected wallet
+    const [entranceFee, setEntranceFee] = useState(0)
+    const chainId = parseInt(chainIdHex)
+    const raffleAddress = chainId in contractAddresses ? contractAddresses[chainId][0] : null
+
+    // const { runContractFunction: enterRaffle } = useWeb3Contract({
+    //     abi,
+    //     contractAddress: raffleAddress,
+    //     functionName: "enterRaffle",
+    //     params: {},
+    //     msgValue, //entranceFee
+    // })
+
+    const { runContractFunction: getEntranceFee } = useWeb3Contract({
+        abi,
+        contractAddress: raffleAddress,
+        functionName: "getEntranceFee",
+        params: {},
+    })
+    useEffect(() => {
+        if (isWeb3Enabled) {
+            //the semi is from the prettier no-semi rule
+            ;(async () => {
+                const entranceFeeFromCall = (await getEntranceFee()).toString()
+                setEntranceFee(ethers.utils.formatUnits(entranceFeeFromCall, "ether"))
+            })()
+        }
+    }, [isWeb3Enabled])
+
+    return <div>Entrance Fee: {entranceFee} ETH</div>
+}
+
+export default LotteryEntrance
